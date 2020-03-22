319eef98f5ad0440e62b9b055adb2b2f677321df
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2020-03-22 13:21:49 +0100

    Add font awesome.

---
+++ Data/Lekarna.Data/Migrations/20200322070950_AddMedicineAndOffersCountFields.cs
@@ -0,0 +1,62 @@
+﻿namespace Lekarna.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AddMedicineAndOffersCountFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Suppliers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Medicine",
+                table: "Offers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OffersCount",
+                table: "Offers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Suppliers_UserId",
+                table: "Suppliers",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Suppliers_AspNetUsers_UserId",
+                table: "Suppliers",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Suppliers_AspNetUsers_UserId",
+                table: "Suppliers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Suppliers_UserId",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Medicine",
+                table: "Offers");
+
+            migrationBuilder.DropColumn(
+                name: "OffersCount",
+                table: "Offers");
+        }
+    }
+}
