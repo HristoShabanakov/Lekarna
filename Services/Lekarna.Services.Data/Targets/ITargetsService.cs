93f99d107654d15833b09129bc73ffabda2b8b30
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2020-09-09 22:03:51 +0200

    Refactor ITargetsService.

--- Services/Lekarna.Services.Data/Targets/ITargetsService.cs
+++ Services/Lekarna.Services.Data/Targets/ITargetsService.cs
@@ -2,13 +2,9 @@
 {
     using System.Threading.Tasks;
 
-<<<<<<< HEAD
-    public interface ITargetsService
-=======
     using Lekarna.Services.Data.Common;
 
     public interface ITargetsService : IService
->>>>>>> 214664bdd1a794b64e54961c0802de472cbe7488
     {
         Task<string> CreateAsync(int quantity);
     }
